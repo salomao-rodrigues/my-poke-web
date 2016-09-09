6927428fe295a0e18705eb801cfdb1679e512bf3
Salomao Rodrigues <salomao88@gmail.com>
2016-09-10 00:56:52 +0100

    Extract production webpack configuration

---
+++ webpack.prod.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var config = {
+  entry: path.join(__dirname, './src/index.jsx'),
+  devServer: {
+    historyApiFallback: true
+  },
+  output: {
+    path: path.join(__dirname, './build/'),
+    filename: 'app.bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CopyWebpackPlugin([
+      {
+        context: path.join(__dirname, '/src/static'),
+        from:'**/*',
+        to :'static'
+      }
+    ])
+  ],
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
